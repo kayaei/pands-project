cd6c752237a62338a1f59f57209a7f7f41d39402
Etem Kaya <etem.kaya@gmail.com>
2019-04-07 21:42:32 +0100

    Updated 'iris_dataset.py' file

--- iris_dataset.py
+++ iris_dataset.py
@@ -1,4 +1,13 @@
 # Etem Kaya 31-Mar-2019
-
 # Irish Data Set Project.
 
+# Analysis of Iris Dataset
+
+# Import pandas library
+import pandas as pd
+
+# Import iris-dataset csv file with panda
+f = pd.read_csv('iris.csv')
+
+# Print the entire iris-datase
+print(f)
