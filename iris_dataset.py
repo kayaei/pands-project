d4622bd2f29fc49c865e1255ea743135e0d915ba
Etem Kaya <etem.kaya@gmail.com>
2019-04-08 00:09:19 +0100

    Added new 'iris_dataset.py' file

---
+++ iris_dataset.py
@@ -0,0 +1,12 @@
+# Etem Kaya 08-Apr-2019
+# Irish Data Set Project.
+
+# Pinrt Iris Dataset in Tabular format
+
+# Import pandas library
+import pandas as pd
+
+# Import dataset with panda
+iris = pd.read_csv('iris.csv')
+
+print(iris) # print the entire dataset in tabular format
