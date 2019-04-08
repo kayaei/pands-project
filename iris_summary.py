9c4e78ad5932c690dfd6253e415b6fc365ded315
Etem Kaya <etem.kaya@gmail.com>
2019-04-08 21:12:03 +0100

    Updated 'iris_summary.py'

--- iris_summary.py
+++ iris_summary.py
@@ -9,10 +9,8 @@ import pandas as pd
 # Import dataset with panda
 f = pd.read_csv('iris.csv')
 
-print("\n" + str(f.columns))                   # column names
-print("\n")
-print("\n" + str(f.info()))                    # information for all columns
-print("\n" + str(f.describe()))                # summary of each column
+print("\n" + str(f.info()))                    # information about the dataset
+print("\n" + str(f.describe()))                # summary of each species
 print("\n" + str(f["species"].value_counts())) # row count in each species
 print("\n" + str(f.shape))                     # total row and column count
 print("\n" + str(f.head()))                    # print first 5 rows
@@ -21,4 +19,5 @@ print("\n" + str(f.sample(5)))                 # output 5 random rows
 
 # reference https://stackoverflow.com/a/29530559
 print("\n" + str(f.isnull().any()))            # find any null values
+print("\n" + str(f.columns))                   # column names
 
