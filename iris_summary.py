86f808a652ea7baaa6c486a64b2e9e83501ba9d7
Etem Kaya <etem.kaya@gmail.com>
2019-04-10 00:40:30 +0100

    Updated 'iris_summary.py' file

--- iris_summary.py
+++ iris_summary.py
@@ -9,17 +9,18 @@ import pandas as pd
 # Import dataset with panda
 f = pd.read_csv('iris.csv')
 
-print("\n" + str(f.info()))                    # information about the dataset
-print("\n" + str(f.describe()))                # summary of each species
-
-# adopted from https://priagungkhusumanegara.github.io/data/Data_Visualization.html
-print("\n" + str(f["species"].value_counts())) # row count in each species
-print("\n" + str(f.shape))                     # total row and column count
-print("\n" + str(f.head()))                    # print first 5 rows
-print("\n" + str(f.tail()))                    # print last 5 rows
-print("\n" + str(f.sample(5)))                 # output 5 random rows
+print(str(f.info()) + "\n") # information about the dataset
+print(str(f.describe()) + "\n") # summary of each species
+print(str(f.shape) + "\n") # total row and column count
+print(str(f.head()) + "\n") # print first 5 rows
+print(str(f.tail()) + "\n") # print last 5 rows
+print(str(f.sample(5)) + "\n") # output 5 random rows
+print((f.columns) + "\n") # column names of the dataset
 
 # adopted from https://stackoverflow.com/a/29530559
-print("\n" + str(f.isnull().any()))            # find any null values
-print("\n" + str(f.columns))                   # column names
+print("\n" + str(f.isnull().any()) + "\n") # find any null values
+
+# adopted from https://priagungkhusumanegara.github.io/data/Data_Visualization.html
+print(f["species"].value_counts()) # row count in each species
+
 
