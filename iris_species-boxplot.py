b009cb2f7e1af351cd8d5e0c9e9753db86d37bbe
Etem Kaya <etem.kaya@gmail.com>
2019-04-09 23:40:27 +0100

    Updated 'iris_species-boxplot.py' file

---
+++ iris_species-boxplot.py
@@ -0,0 +1,17 @@
+#%%
+# Etem Kaya 09-Apr-2019
+# Irish Data Set Project.
+
+# Boxplot iris dataset
+# import pandas and seaborn libraries
+import pandas as pd
+import seaborn as sns
+
+# set style and colour using seaborn library
+sns.set(style="darkgrid", color_codes=True)
+
+# import iris dataset with Pandas
+f = pd.read_csv("iris.csv")
+
+# boxplot grouped by species
+f.boxplot(by='species',figsize=(12,8))
