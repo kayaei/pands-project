c948dd777da6d6c1642a22b7be6cb09114964747
Etem Kaya <etem.kaya@gmail.com>
2019-04-09 23:24:22 +0100

    Added 'iris_sepal-length-boxplot' .py and .jpg files

---
+++ iris_sepal-length-boxplot.py
@@ -0,0 +1,16 @@
+#%%
+# Etem Kaya 09-Apr-2019
+# Irish Data Set Project.
+
+# import pandas and seaborn libraries
+import pandas as pd
+import seaborn as sns
+
+# set style and colour using seaborn library
+sns.set(style="darkgrid", color_codes=True)
+
+# import iris dataset with Pandas
+f = pd.read_csv("iris.csv")
+
+# boxplot sepal lenght
+sns.boxplot(x="species", y="sepal_length", data=f)
