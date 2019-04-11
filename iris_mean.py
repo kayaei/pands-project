38ca366154d67b00eb24575d22011278d16a5c6f
Etem Kaya <etem.kaya@gmail.com>
2019-04-11 22:19:06 +0100

    Updated 'iris_mean.py' file

--- iris_mean.py
+++ iris_mean.py
@@ -1,15 +1,16 @@
 #%%
 
 # Etem Kaya 11-Apr-2019
-# Irish Data Set Project.
+# Iris Data Set Project.
 # Analysing Iris Dataset with 'mean, max, min and median' functions
 
-import pandas as pd #pandas module imported
-import seaborn as sns
+import pandas as pd # import pandas library
+import seaborn as sns # import seaborn library
 
-# f = pd.read_csv("iris.csv") #import dataset with panda
-f = sns.load_dataset("iris") #import dataset with seaborn
+# f = pd.read_csv("iris.csv") # import dataset with panda
+f = sns.load_dataset("iris") # this time import dataset with seaborn
 
+# finding the mean of each attribute
 print("Mean of Sepal Length is", f["sepal_length"].mean())
 print("\n")
 print("Mean of Sepal Width is", f["sepal_width"].mean())
@@ -19,20 +20,10 @@ print("\n")
 print("Mean of Petal Width is", f["petal_width"].mean())
 print("\n")
 
+# group the iris dataset by the species column
 group = f.groupby("species")
 
+# find the mean of each attribute for all 3 species
 print("Mean of all 3 Species")
 print(group.mean())
-print("\n")
-
-print("Max value of all 3 Species")
-print(group.max())
-print("\n")
-
-print("Min value of all 3 Species")
-print(group.min())
-print("\n")
-
-print("Median of all 3 Species")
-print(group.median())
 print("\n")
