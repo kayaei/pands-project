0b81000e36f6b69921f686ba54e6042aa43965c5
Etem Kaya <etem.kaya@gmail.com>
2019-04-11 23:19:57 +0100

    Updated 'iris_mean.py' file

--- iris_mean.py
+++ iris_mean.py
@@ -10,21 +10,28 @@ import seaborn as sns # import seaborn library
 # f = pd.read_csv("iris.csv") # import dataset with panda
 f = sns.load_dataset("iris") # this time import dataset with seaborn
 
-# finding the mean of each attribute
+# finding mean of each attribute with pandas
+print("Finding mean using pandas", "\n")
 print("Mean of Sepal Length is", f["sepal_length"].mean())
 print("Mean of Sepal Width is", f["sepal_width"].mean())
 print("Mean of Petal Length is", f["petal_length"].mean())
 print("Mean of Petal Width is", f["petal_width"].mean())
+print("\n")
 
 # group the iris dataset by the species category
 group = f.groupby("species")
 
-# find the mean of each attribute for all 3 species
+# finding mean of each attribute for all 3 species using group function
+print("Finding mean using 'group' function", "\n")
+
 print("Mean of all 3 Species")
 print(group.mean())
 print("\n")
 
-# another way of finding th emean by splitting the dataset with loc function
+# finding mean by splitting the dataset with loc function
+
+print("Finding mean of species using 'loc' function", "\n")
+
 print("Mean of Setosa species")
 print(f.loc[f["species"]=="setosa"].mean()) # mean of setosa using loc function
 print("\n")
@@ -34,6 +41,9 @@ print("\n")
 print("Mean of Virginica species")
 print(f.loc[f["species"]=="virginica"].mean()) # mean of virginica using loc function
 print("\n")
+
+# another way of finding mean by splitting the dataset with iloc function
+print("Finding mean of species using 'iloc' function", "\n")
 
 print("Mean of Setosa species")
 print(f.iloc[0:50].mean()) # mean of setosa using iloc function
