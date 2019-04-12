0c05259f30da0afecdba96a3edb59d77885c0c96
Etem Kaya <etem.kaya@gmail.com>
2019-04-12 23:40:16 +0100

    Added 'iris_scatterplot.py' file

---
+++ iris_scatterplot.py
@@ -0,0 +1,39 @@
+#%%
+# Etem Kaya 12-Apr-2019
+# Iris Data Set Project.
+
+# Histogram iris dataset
+
+# import pandas, seaborn and matplotlib libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+# import iris dataset with Pandas
+f = pd.read_csv("iris.csv")
+
+# set style and background colour using seaborn library
+sns.set(style="darkgrid")
+
+sns.distplot(f["sepal_length"])
+plt.title('Histogram - Sepal Length of All Species')
+plt.xlabel('Sepal_Length')
+plt.ylabel('count')
+plt.show()
+
+sns.distplot(f["sepal_width"])
+plt.title('Histogram - Sepal Width of All Species')
+plt.xlabel('Sepal_Width')
+plt.ylabel('count')
+plt.show()
+
+sns.distplot(f["petal_length"])
+plt.title('Histogram - Petal Length of All Species')
+plt.xlabel('Petal_Length')
+plt.ylabel('count')
+plt.show()
+
+sns.distplot(f["petal_width"])
+plt.title('Histogram - Petal Width of All Species')
+plt.xlabel('Petal_Width')
+plt.ylabel('count')
+plt.show()
