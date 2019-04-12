17531f0acb04b7fff088b522ed60fbbdae8cef98
Etem Kaya <etem.kaya@gmail.com>
2019-04-12 23:54:02 +0100

    Added 'iris_scatterplot.py'

---
+++ iris_scatterplot.py
@@ -0,0 +1,10 @@
+#%%
+
+# Etem Kaya 12-Apr-2019
+# Iris Data Set Project.
+# Scatterplot graphs of Iris Dataset
+
+# import pandas, seaborn and matplotlib libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
