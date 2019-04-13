a5a0c59de8dc3b6ba92f7d991153f93d8adcff97
Etem Kaya <etem.kaya@gmail.com>
2019-04-13 16:21:23 +0100

    Updated 'iris_setosa-histogram.py'

--- iris_setosa-histogram.py
+++ iris_setosa-histogram.py
@@ -18,35 +18,35 @@ Setosa = f.loc[f["species"]=="setosa"]
 # set style and background colour using seaborn library
 sns.set(style="whitegrid")
 
-# Histogram of Setosa Sepal Length
+# Histogram of Setosa 'Sepal Length'
 sns.distplot(Setosa["sepal_length"], color= "green")
 plt.title('Histogram - Sepal Length of Setosa')
 plt.xlabel('Setosa Sepal Length')
 plt.ylabel('count')
 plt.show()
 
-# Histogram of Setosa Sepal Width
+# Histogram of Setosa 'Sepal Width'
 sns.distplot(Setosa["sepal_width"], color= "blue")
 plt.title('Histogram - Sepal Width of Setosa')
 plt.xlabel('Setosa Sepal Width')
 plt.ylabel('count')
 plt.show()
 
-# Histogram of Setosa Petal Length
+# Histogram of Setosa 'Petal Length'
 sns.distplot(Setosa["petal_length"], color= "orange")
 plt.title('Histogram - Petal Length of Setosa')
 plt.xlabel('Setosa Petal Length')
 plt.ylabel('count')
 plt.show()
 
-# Histogram of Setosa Petal Width
+# Histogram of Setosa 'Petal Width'
 sns.distplot(Setosa["petal_width"], color= "red")
 plt.title('Histogram - Petal Width of Setosa')
 plt.xlabel('Setosa Petal Width')
 plt.ylabel('count')
 plt.show()
 
-# Histogram of Setosa Sepal Length and Sepal Width
+# Histogram of Setosa 'Sepal Length and Sepal Width'
 sns.distplot(Setosa["sepal_length"], color= "green")
 sns.distplot(Setosa["sepal_width"], color= "blue")
 plt.title('Histogram - Setosa Sepal Length and Width')
@@ -54,7 +54,7 @@ plt.xlabel('Setosa Sepal Length and Width')
 plt.ylabel('count')
 plt.show()
 
-# Histogram of Setosa Petal Length and Petal Width
+# Histogram of Setosa 'Petal Length and Petal Width'
 sns.distplot(Setosa["petal_length"], color= "orange")
 sns.distplot(Setosa["petal_width"], color= "red")
 plt.title('Histogram - Setosa Petal Length and Width')
