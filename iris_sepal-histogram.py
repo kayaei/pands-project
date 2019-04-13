9a8d3f817eec36964afa4984962bbf853c7d145f
Etem Kaya <etem.kaya@gmail.com>
2019-04-13 16:59:21 +0100

    Updated 'iris_sepal-histogram.py'

--- iris_sepal-histogram.py
+++ iris_sepal-histogram.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 # import iris dataset with Pandas
 f = pd.read_csv("iris.csv")
 
-# Split Setosa, Versicolor and Virginica dataset from the entire Iris dataset
+# split 'Setosa, Versicolor and Virginica' dataset from the entire Iris dataset
 Setosa = f.loc[f["species"]=="setosa"]
 Versicolor = f.loc[f["species"]=="versicolor"]
 Virginica = f.loc[f["species"]=="virginica"]
@@ -20,21 +20,21 @@ Virginica = f.loc[f["species"]=="virginica"]
 # set style and background colour using seaborn library
 sns.set(style="darkgrid")
 
-# Histogram of Setosa, Versicolor and Virginica 'sepal Length'
+# Histogram of 'Setosa, Versicolor and Virginica' Sepal Length
 sns.distplot(Setosa["sepal_length"], color= "green")
 sns.distplot(Versicolor["sepal_length"], color= "blue")
 sns.distplot(Virginica["sepal_length"], color= "orange")
-plt.title('Histogram - sepal Length of Setosa, Versicolor and Virginica')
-plt.xlabel('sepal_Length' + "\nSetosa, Versicolor and Virginica")
+plt.title("Histogram - Sepal Length of 'Setosa, Versicolor and Virginica'")
+plt.xlabel('Sepal_Length' + "\nSetosa, Versicolor and Virginica")
 plt.ylabel('count')
 plt.show()
 
-# Histogram of Setosa, Versicolor and Virginica 'sepal Width'
+# Histogram of 'Setosa, Versicolor and Virginica' Sepal Width
 sns.distplot(Setosa["sepal_width"], color= "green")
 sns.distplot(Versicolor["sepal_width"], color= "blue")
 sns.distplot(Virginica["sepal_width"], color= "orange")
-plt.title('Histogram - sepal Width of Setosa, Versicolor and Virginica')
-plt.xlabel('sepal_Width' + "\nSetosa, Versicolor and Virginica")
+plt.title("Histogram - Sepal Width of 'Setosa, Versicolor and Virginica'")
+plt.xlabel('Sepal_Width' + "\nSetosa, Versicolor and Virginica")
 plt.ylabel('count')
 plt.show()
 
