485e381cba5957d1cad03d670c08546f7651cf25
Etem Kaya <etem.kaya@gmail.com>
2019-04-13 19:19:26 +0100

    Renamed and updated 'iris_boxplot-sepal.py' file

--- iris_boxplot-sepal.py
+++ iris_boxplot-sepal.py
@@ -2,9 +2,10 @@
 # Etem Kaya 09-Apr-2019
 # Iris Data Set Project.
 
-# import pandas and seaborn libraries
+# import pandas, seaborn and matplotlib libraries
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # set style and colour using seaborn library
 sns.set(style="ticks", color_codes=True)
@@ -14,3 +15,8 @@ f = pd.read_csv("iris.csv")
 
 # boxplot sepal lenght
 sns.boxplot(x="species", y="sepal_length", data=f)
+plt.show()
+
+# boxplot sepal width
+sns.boxplot(x="species", y="sepal_width", data=f)
+plt.show()
