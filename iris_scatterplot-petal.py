3c80c0c6b0f0176bfe22561c9eb2929433206845
Etem Kaya <etem.kaya@gmail.com>
2019-04-13 20:08:09 +0100

    Updated 'iris_scatterplot-sepal.py'

--- iris_scatterplot-petal.py
+++ iris_scatterplot-petal.py
@@ -2,7 +2,7 @@
 
 # Etem Kaya 12-Apr-2019
 # Iris Data Set Project.
-# Scatterplots for the 'Sepal' category of the Iris dataset
+# Scatterplots for the 'Petal' category of the Iris dataset
 
 # import pandas, seaborn and matplotlib libraries
 import pandas as pd
@@ -17,19 +17,19 @@ Setosa = f.loc[f["species"]=="setosa"]
 Versicolor = f.loc[f["species"]=="versicolor"]
 Virginica = f.loc[f["species"]=="virginica"]
 
-# set the parameters for the 'Setosa - Sepal' scatter plot
-ax = sns.scatterplot(x="sepal_length", y="sepal_width", hue="species", data=Setosa)
+# set the parameters for the Setosa - Petal scatter plot
+ax = sns.scatterplot(x="petal_length", y="petal_width", hue="species", data=Setosa)
 # set the title of the scatter plot
 plt.title("Scatter Plot - 'Sepal Length and Sepal Width' of Setosa")
 plt.show()
 
-# set the parameters for the 'Versicolor - Sepal' scatter plot
+# set the parameters for the Versicolor - Petal scatter plot
 ax = sns.scatterplot(x="sepal_length", y="sepal_width", hue="species", data=Versicolor)
 # set the title of the scatter plot
 plt.title("Scatter Plot - 'Sepal Length and Sepal Width' of Versicolor")
 plt.show()
 
-# set the parameters for the 'Virginica - Sepal' scatter plot
+# set the parameters for the Virginica - Petal scatter plot
 ax = sns.scatterplot(x="sepal_length", y="sepal_width", hue="species", data=Virginica)
 # set the title of the scatter plot
 plt.title("Scatter Plot - 'Sepal Length and Sepal Width' of Virginica")
