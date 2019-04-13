b7c31ad23c32067fcd4b07da96bf9ca39c921a76
Etem Kaya <etem.kaya@gmail.com>
2019-04-13 19:33:49 +0100

    Renamed and updated 'iris_boxplot-petal.py' and 'iris_boxplot-petal.jpg' files

--- iris_boxplot-petal.py
+++ iris_boxplot-petal.py
@@ -2,9 +2,10 @@
 # Etem Kaya 09-Apr-2019
 # Irish Data Set Project.
 
-# import pandas and seaborn libraries
+# import pandas, seaborn and matplotlib libraries
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # set style and colour using seaborn library
 sns.set(style="darkgrid", color_codes=True)
@@ -14,3 +15,8 @@ f = pd.read_csv("iris.csv")
 
 # boxplot petal lenght
 sns.boxplot(x="species", y="petal_length", data=f)
+plt.show()
+
+# boxplot petal width
+sns.boxplot(x="species", y="petal_width", data=f)
+plt.show()
